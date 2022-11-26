f208eb27545b6a2d97e92bf793c7ed5162e17239
pratxks <101081557+pratxks@users.noreply.github.com>
2022-11-26 16:55:42 +0530

    finished: getRandomWord class

--- resources/js/Hangman.js
+++ resources/js/Hangman.js
@@ -25,7 +25,7 @@ class Hangman {
    * */
   getRandomWord(difficulty) {
     return fetch(
-      `https://hangman-micro-service-bpblrjerwh.now.sh?difficulty=${difficulty}`
+      "https://hangman-micro-service.herokuapp.com/?difficulty=${difficulty}"
     )
       .then((r) => r.json())
       .then((r) => r.word);
